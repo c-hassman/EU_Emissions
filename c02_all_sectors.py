049ef09b2654663717154cb09b454171de310a2a
Colburn Hassman <colburn7@vt.edu>
2020-05-31 10:37:25 +0200

    Renamed previous python analysis, modified the dataset by removing the note at the bottom describing the na value, shortened the germany label, recreated previous work using the correct dataset.

--- c02_all_sectors.py
+++ c02_all_sectors.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat May 30 10:55:14 2020
+Created on Sun May 31 10:07:52 2020
 
 @author: Colburn Hassman and Ingrid Nilsson
 """
@@ -10,20 +10,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #%%
-# Read in Excel File and make "Long"
-data = pd.read_excel('total_c02.xls', skiprows = 10, na_values = ":") # Read in excel, skip header rows, : = na 
+data = pd.read_excel('air_emmis.xls', skiprows = 10, na_values = ":") # Read in excel, skip header rows, : = na 
 data = pd.melt(data, id_vars = ["GEO/TIME"], var_name = 'year') # Melt data to make narrow/long
-print(data)
+data = data.set_index(["GEO/TIME", "year"]) #Set Index to a multi index of country and year
 #%%
-data = data.set_index(["GEO/TIME", "year"])
-
+plt.plot(data.loc['Germany'], label = "Germany")
+plt.plot(data.loc['France'], label = "France")
+plt.title("Total Co2 Emmissions per Year")
+plt.ylabel("Tonnes Co2")
+plt.legend()
 #%%
-print(type(data))
-
-print(data.loc['Sweden'])
-
-#%%
-print(data.loc['Denmark'].describe())
-
-#%%
-plt.plot(data.loc['Sweden'])
